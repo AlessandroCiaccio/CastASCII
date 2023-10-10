14ca3fa72dfcc79738d738974ba9be378e5f47f0
Alessandro Scrofani <sandroscro@libero.it>
2023-10-10 18:28:58 +0200

    Added Main

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String [] arg){
+        char x='g';
+        char y='b';
+        System.out.print("The sum in ASCII of " + x + " + " + y + " is: " + (int)x+(int)y);
+    }
+}
